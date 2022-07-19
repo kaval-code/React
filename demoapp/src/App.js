52f5c650b9fd7ca9030725270c52fa85eab214a2
kaval-code <180320107077.ce.kavalkumar@gmail.com>
2022-07-19 10:14:05 +0530

    array

--- demoapp/src/App.js
+++ demoapp/src/App.js
@@ -5,6 +5,7 @@ import Data from "./Data";
 const App = () => {
     return (
         <>
+
             <Card
                 name={Data[0].name}
                 imgsrc={Data[0].imgsrc}
@@ -12,15 +13,15 @@ const App = () => {
             />
 
             <Card
-                name={Data[1].name}
-                imgsrc={Data[1].imgsrc}
-                info={Data[1].info}
-            />
-
-            <Card
                 name={Data[2].name}
                 imgsrc={Data[2].imgsrc}
                 info={Data[2].info}
+            />
+
+            <Card
+                name={Data[1].name}
+                imgsrc={Data[1].imgsrc}
+                info={Data[1].info}
             />
         </>
     );
